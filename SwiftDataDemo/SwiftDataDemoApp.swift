078abbb68cd2d21bc37ec14db21cc5769dcda9f5
Jassbhullar02 <jassbhullar1502@gmail.com>
2025-10-04 16:41:57 +0530

    Start SwiftData Project

---
+++ SwiftDataDemo/SwiftDataDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataDemoApp.swift
+//  SwiftDataDemo
+//
+//  Created by Jaspreet Bhullar on 04/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
